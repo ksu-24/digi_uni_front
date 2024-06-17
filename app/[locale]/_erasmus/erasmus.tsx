d6eceaf4a987de20b2178a48993207ba2f511e8d
Evemose <rotan.olexandr@lll.kpi.ua>
2024-06-17 17:52:55 +0300

    erasmus fixes

--- app/[locale]/_erasmus/erasmus.tsx
+++ app/[locale]/_erasmus/erasmus.tsx
@@ -1,11 +1,16 @@
-import {Stack} from "@mui/material";
+import {Stack, Typography} from "@mui/material";
 import Text from "@/app/[locale]/_erasmus/text";
 import Cards from "@/app/[locale]/_erasmus/cards";
+import {getTranslations} from "next-intl/server";
 
 export default async function Erasmus() {
+    const translations = await getTranslations("main.about");
     return (
-        <Stack className="gap-[10%] w-full">
-            <Text />
+        <Stack className="w-full">
+            <Stack className="w-full px-[11%] pt-[10%] h-dvh gap-[6dvh]">
+                <Typography variant="body1">01 {translations("enumerationCaption")}</Typography>
+                <Text/>
+            </Stack>
             <Cards/>
         </Stack>
     )
--- app/[locale]/_erasmus/text.tsx
+++ app/[locale]/_erasmus/text.tsx
@@ -4,10 +4,9 @@ import {Stack, Typography} from "@mui/material";
 export default async function Text() {
     const translations = await getTranslations("main.about");
     return (
-        <Stack className="w-full h-dvh px-[11%] pt-[10%]">
-            <Typography variant="body1">01 {translations("enumerationCaption")}</Typography>
+        <>
             <Typography variant="h3" className="w-3/5">{translations("title")}</Typography>
             <Typography variant="body2" className="whitespace-pre-wrap">{translations("content")}</Typography>
-        </Stack>
+        </>
     );
 }
